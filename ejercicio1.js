d03bff6e052f579a58354da25e480f6b6162aaf5
DiegoMolina <134454332+DiegoFMolina@users.noreply.github.com>
2023-06-19 17:28:21 -0500

    cree una funcion que recibe un numero entero y devuelve el numero palindromo siguiente a ese numero (#1)

---
+++ ejercicio1.js
@@ -0,0 +1,11 @@
+function next_pal(val) {
+    for (let i = val + 1; ; i++) {
+        if (String(i) == String(i).split("").reverse().join("")) {
+             console.log(i);
+        return i;
+       
+        }
+    }
+}
+
+next_pal(77)
